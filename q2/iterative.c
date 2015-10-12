f6f4e2135c4f3dd3549ac5db30e53667ebf71811
mimihalo <mimihalo@miko.tw>
2015-10-12 14:33:00 +0800

    first commit q2
    first commit both version of q2

--- q2/iterative.c
+++ q2/iterative.c
@@ -1,6 +1,29 @@
-/* FIXME: Implement! */
+#include <stdio.h>
+
+char smallest_character(char str[], char c)
+{
+    int i=0;
+    while(str[i]!='\0') {
+        if(str[i]>c)
+            return str[i];
+        i++;
+    }
+    return str[0];
+}
 
 int main()
 {
+    char str[128];
+    char c;
+    char res;
+    printf("str: ");
+    gets(str);
+    printf("c: ");
+    scanf("%c",&c);
+    fflush(stdin);
+
+    res=smallest_character(str, c);
+    printf("%c\n",res);
+
     return 0;
 }
--- q2/recursive.c
+++ q2/recursive.c
@@ -1,6 +1,35 @@
-/* FIXME: Implement! */
+#include <stdio.h>
+
+char smallest_character(char str[], char c)
+{
+    char tmp;
+    if(str[0]!='\0') {
+        if(str[0]>c)
+            return str[0];
+        else {
+            tmp=smallest_character(str+1,c);
+            if(tmp<=c)
+                return str[0];
+            else
+                return tmp;
+        }
+    }
+    return 0;
+}
 
 int main()
 {
+    char str[128];
+    char c;
+    char res;
+    printf("str: ");
+    gets(str);
+    printf("c: ");
+    scanf("%c",&c);
+    fflush(stdin);
+
+    res=smallest_character(str, c);
+    printf("%c\n",res);
+
     return 0;
 }
