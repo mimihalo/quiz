3f5ea18921b72fd3e6f16731737ba8bc9d66fd69
mimihalo <mimihalo@miko.tw>
2015-10-14 16:56:27 +0800

    first commit q4
    commit both version of q4

--- q4/iterative.c
+++ q4/iterative.c
@@ -1,6 +1,65 @@
-/* FIXME: Implement! */
+#include <stdio.h>
+
+int maxSubArray(int A[], int n)
+{
+    int sum=0;
+    int maxsum=0x80000000;
+
+    for(int range=1,i; range<=n; range++) {
+        sum=0;
+        for(i=0; i<range; i++) {
+            sum+=A[i];
+        }
+        while(i<n) {
+            if(sum>maxsum)
+                maxsum=sum;
+
+            sum+=A[i];
+            sum-=A[i-range];
+            i++;
+        }
+    }
+
+    return maxsum;
+}
+
+int *init_array(int *n)
+{
+    int cnt=0,tmp,i=0;
+    int *res;
+    char strtmp[16];
+    FILE *fp=fopen("testdata","r");
+
+    while(fscanf(fp,"%s",&strtmp)==1)
+        cnt++;
+
+    fclose(fp);
+    res=calloc(cnt,sizeof(int));
+    fp=fopen("testdata","r");
+
+    while(fscanf(fp,"%s",&strtmp)==1) {
+        tmp=atoi(strtmp);
+        res[i]=tmp;
+        i++;
+    }
+    *n=cnt;
+    return res;
+}
+
+void print_arr(int *a,int n)
+{
+    for(int i=0; i<n; i++) {
+        printf("%d ",a[i]);
+    }
+    printf("\n");
+}
 
 int main()
 {
+    int n;
+    int *arr=init_array(&n);
+    print_arr(arr,n);
+
+    printf("maxSum = %d\n",maxSubArray(arr,n));
     return 0;
 }
--- q4/recursive.c
+++ q4/recursive.c
@@ -1,6 +1,66 @@
-/* FIXME: Implement! */
+#include <stdio.h>
 
-int main()
+int maxSubArray(int *A, int n)
 {
-    return 0;
+	int sum=0;
+	int maxsum=0x80000000;
+	int maxsum2=0x80000000;
+	
+	if(n-1>0)
+		maxsum2=maxSubArray(A+1,n-1);
+	
+	for(int i=0;i<n;i++)
+	{
+		sum+=A[i];
+		if(sum>maxsum)
+			maxsum=sum;
+	}
+	
+	if(maxsum>maxsum2)
+		return maxsum;
+	else
+		return maxsum2;
+}
+
+int *init_array(int *n)
+{
+	int cnt=0,tmp,i=0;
+	int *res;
+	char strtmp[16];
+	FILE *fp=fopen("testdata","r");
+	
+	while(fscanf(fp,"%s",&strtmp)==1)
+		cnt++;
+	
+	fclose(fp);
+	res=calloc(cnt,sizeof(int));
+	fp=fopen("testdata","r");
+	
+	while(fscanf(fp,"%s",&strtmp)==1)
+	{
+		tmp=atoi(strtmp);
+		res[i]=tmp;
+		i++;
+	}
+	*n=cnt;
+	return res;
+}
+
+void print_arr(int *a,int n)
+{
+	for(int i=0;i<n;i++)
+	{
+		printf("%d ",a[i]);
+	}
+	printf("\n");
+}
+
+int main() 
+{
+	int n;
+	int *arr=init_array(&n);
+	print_arr(arr,n);
+	
+	printf("maxSum = %d\n",maxSubArray(arr,n));
+	return 0;
 }
